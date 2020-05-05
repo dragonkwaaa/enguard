5f8236941015f8fbaac4f6e1fc7458583f378976
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-05-05 21:14:23 +0900

    Create head.php

---
+++ [[specialSave]]/풀페이지 적용한 인덱스 파일 보존/20200505_2103/nGuard/common/pages/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+	<title>N_Guard</title>
+
+	<link type="text/css" rel="stylesheet" href="/nGuard/common/css/reset.css">
+	<link type="text/css" rel="stylesheet" href="/nGuard/common/css/ui.css">
+	<link type="text/css" rel="stylesheet" href="/nGuard/common/css/style.css">
+	<link type="text/css" rel="stylesheet" href="/nGuard/common/css/fullpage-2.8.6.css">
+	<link type="text/css" rel="stylesheet" href="/nGuard/common/css/customFullpage.css">
+	
+	<script src="/nGuard/common/js/jquery-3.1.1.min.js"></script>
+	<script src="/nGuard/common/js/jquery-ui-1.12.1.js"></script>
+	<script src="/nGuard/common/js/scrolloverflow.min.js"></script>
+	<script src="/nGuard/common/js/fullpage-2.8.6.js"></script>
+
+
+
+
+
+</head>
+<body>
