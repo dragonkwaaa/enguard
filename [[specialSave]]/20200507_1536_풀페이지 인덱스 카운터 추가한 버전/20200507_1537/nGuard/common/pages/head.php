e193f01bb5640c209046e23cc82ae20625512496
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-05-07 15:42:29 +0900

    Add files via upload

---
+++ [[specialSave]]/20200507_1536_풀페이지 인덱스 카운터 추가한 버전/20200507_1537/nGuard/common/pages/head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+	<title>N_Guard</title>
+
+	<link type="text/css" rel="stylesheet" href="/nGuard/common/css/reset.css">
+	<link type="text/css" rel="stylesheet" href="/nGuard/common/css/fullpage-2.8.6.css">
+	<link type="text/css" rel="stylesheet" href="/nGuard/common/css/customFullpage.css">
+	<link rel="stylesheet" type="text/css" href="/nonghyup/common/css/slick-theme.css">
+	<link rel="stylesheet" type="text/css" href="/nonghyup/common/css/slick.css">
+	<link type="text/css" rel="stylesheet" href="/nGuard/common/css/ui.css">
+	<link type="text/css" rel="stylesheet" href="/nGuard/common/css/style.css">
+	
+	<script src="/nGuard/common/js/jquery-3.1.1.min.js"></script>
+	<script src="/nGuard/common/js/jquery-ui-1.12.1.js"></script>
+	<script src="/nGuard/common/js/scrolloverflow.min.js"></script>
+	<script src="/nGuard/common/js/fullpage-2.8.6.js"></script>
+	<script src="/nonghyup/common/js/slick.js"></script>
+
+</head>
+<body>
